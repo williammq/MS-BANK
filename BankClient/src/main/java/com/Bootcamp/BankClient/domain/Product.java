afc77d2b01f76ee5ba8bf4e76e9ed52d51a81045
williammq <williammq@outlook.com>
2022-09-23 14:08:59 -0500

    change configuration

--- BankClient/src/main/java/com/Bootcamp/BankClient/domain/Product.java
+++ BankClient/src/main/java/com/Bootcamp/BankClient/domain/Product.java
@@ -10,11 +10,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class Product {
     
     private String codeProduct;
